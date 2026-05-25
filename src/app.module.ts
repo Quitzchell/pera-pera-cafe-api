43cd43697dfcf90e43e1ef853316df28ce0d4600
mitchellquitz <mitchellquitz@gmail.com>
2026-05-25 20:55:48 +0200

    chore(app): drop duplicate SessionsController registration
    SessionsModule already declares the controller, so listing it
    again on AppModule made Nest try to instantiate it twice.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SessionsController } from './sessions/sessions.controller';
 import { ParticipantsModule } from './participants/participants.module';
 import {SessionsModule} from "./sessions/sessions.module";
 
 @Module({
   imports: [ParticipantsModule, SessionsModule],
-  controllers: [AppController, SessionsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
